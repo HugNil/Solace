daa3168e6c249d369ed37659efa2e3f20033c314
Pontus <157807027+Ponhagen@users.noreply.github.com>
2024-04-26 23:07:08 +0200

    Firbase connection mood

---
+++ firebase_mood.py
@@ -0,0 +1,37 @@
+"""
+Handles firebase connection
+"""
+import firebase_admin
+from firebase_admin import credentials, firestore 
+
+class FirebaseConnection(self):
+    __init__(self):
+    cred = credentials.Certificate(r"Solace_key.json")
+    firebase_admin.initialize_app(cred)
+
+
+
+
+def read_user_mood(self, email):
+    """
+    Read mood data for user
+    """
+    doc_ref = self.db.collection(u'users').document(email)
+    mood_collection = doc_ref.collection(u'mood-form')
+    docs = mood_collection.stream()
+
+    mood_data = []
+    for doc in docs:
+        mood_data.append(doc.to_dict())
+
+    return mood_data
+
+fbc = FirebaseConnection()
+email = '02hugo.nilsson@gmail.com'
+mood_data = fbc.read_user_mood(email)
+
+if mood_data:
+    print(mood_data)
+
+else:
+    print(' No mood data found for this user')
