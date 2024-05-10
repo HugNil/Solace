3b22739d47fef13a178314dd79845f96f945ef99
jacob hellgren <hellgrenjacob@hotmail.com>
2024-05-10 15:25:29 +0200

    Added play and stop function to settingsclass

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import tkinter as tk
 from PIL import Image
+import pygame
 
 
 class Settings():
@@ -12,21 +13,35 @@ class Settings():
         self.app = app
         self.settings_frame = None
         self.app.iconbitmap("assests/Solace logo1_klippt.ico")
-        
 
+    
+    def stop(self):
+        """
+        Stops the menu music
+        """
+        pygame.mixer.music.stop()
+
+
+    def play(self):
+        """
+        Plays the menu music
+        """
+        pygame.mixer.music.load("assests/Menu music1.mp3")
+        pygame.mixer.music.play(loops=1)
+
+        pygame.mixer.music.set_volume(0.009)
+        
 
     def create_frames(self):
         self.settings_frame = ctk.CTkFrame(
             master=self.app,
-            fg_color=self.props.BACKGROUND_DARK,
+            bg=self.props.BACKGROUND_DARK,
             border_color=self.props.BACKGROUND_LIGHT,
             border_width=2,
             width=500,
-            height=800
-
-            )
+            height=800)
         
-        
+                                          
     def open_settings(self):
         settings_window = ctk.CTkToplevel(self.app)
         settings_window .title("Settings")
