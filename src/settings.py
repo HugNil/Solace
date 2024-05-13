3b9769a837d00d34f9cdf146f51ebe322e3affee
jacob hellgren <hellgrenjacob@hotmail.com>
2024-05-13 15:35:34 +0200

    Correct color and shape

--- src/settings.py
+++ src/settings.py
@@ -2,51 +2,33 @@ import customtkinter as ctk
 import tkinter as tk
 from PIL import Image
 import pygame
+from props import Props
 
 
-class Settings():
-    
+class Settings:
+    """
+    Settings page of the application.
+    """
     def __init__(self, app, props, return_to_gui):
-        
         self.props = props
         self.return_to_gui = return_to_gui
+
         self.app = app
-        self.settings_frame = None
-        self.app.iconbitmap("assests/Solace logo1_klippt.ico")
-
-    
-    def stop(self):
-        """
-        Stops the menu music
-        """
-        pygame.mixer.music.stop()
-
-
-    def play(self):
-        """
-        Plays the menu music
-        """
-        pygame.mixer.music.load("assests/Menu music1.mp3")
-        pygame.mixer.music.play(loops=1)
-
-        pygame.mixer.music.set_volume(0.009)
-        
 
     def create_frames(self):
+        self.settings_window = ctk.CTkToplevel(self.app)
+        self.settings_window .title("Settings")
         self.settings_frame = ctk.CTkFrame(
-            master=self.app,
-            bg=self.props.BACKGROUND_DARK,
+            master=self.settings_window,
+            fg_color=self.props.BACKGROUND_DARK,
             border_color=self.props.BACKGROUND_LIGHT,
-            border_width=2,
-            width=500,
-            height=800)
-        
-                                          
+            border_width=2
+            )
+        self.settings_frame.configure(width=int(self.props.WIDTH * 0.8),
+                                      height=int(self.props.HEIGHT * 0.5))
+
     def open_settings(self):
-        settings_window = ctk.CTkToplevel(self.app)
-        settings_window .title("Settings")
-        settings_window.resizable(False, False)
-        settings_window = Settings(self.app, self.props, self.return_to_gui)
-        settings_window.create_frames()
-        
-    
+        self.create_frames()
+        self.settings_window.resizable(False, False)
+        self.settings_frame.pack(fill=tk.BOTH,
+                                 expand=True)
